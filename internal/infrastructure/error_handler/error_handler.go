792dee410b2c104b43374d845642bb5c85ed8d1c
MuriloFlores <muriloaflore@gmail.com>
2025-03-11 22:19:12 -0300

    refactor(error_handler): update DTO definitions and adjust related structures

--- internal/infrastructure/error_handler/error_handler.go
+++ internal/infrastructure/error_handler/error_handler.go
@@ -3,13 +3,13 @@ package error_handler
 import (
 	"encoding/json"
 	"net/http"
-	"store-manager/internal/application/DTOs"
+	"store-manager/internal/application/DTOs/error_DTO"
 )
 
 func WriteJSONError(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(DTOs.ErrorResponse{
+	json.NewEncoder(w).Encode(error_DTO.ErrorResponse{
 		Code:    status,
 		Message: message,
 	})
