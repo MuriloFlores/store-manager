f22fbc7ae27c8c0f1fd7cc8b7bb6043527ec9786
MuriloFlores <muriloaflore@gmail.com>
2025-03-11 22:17:47 -0300

    feat(repositories): create interface for product-raw material repository

--- internal/domain/repositories/product_assoc_raw_material_repository_interface.go
+++ internal/domain/repositories/product_assoc_raw_material_repository_interface.go
@@ -1,16 +1,17 @@
 package repositories
 
 import (
-	"github.com/google/uuid"
-	"store-manager/internal/application/DTOs"
+	"store-manager/internal/application/DTOs/product_assoc_raw_material_DTO"
+	"store-manager/internal/domain/entity"
 )
 
 type ProductAssocRawMaterialRepositoryInterface interface {
-	CreateAssociation(assoc DTOs.ProductAssocRawMaterialDTO) error
-	UpdateAssociation(assoc DTOs.ProductAssocRawMaterialDTO) error
-	DeleteAssociation(productId, materialId uuid.UUID) error
-	GetAssociation(productId, materialId uuid.UUID) (DTOs.ProductAssocRawMaterialDTO, error)
-	ListAssociationByProduct(productId uuid.UUID) ([]DTOs.ProductAssocRawMaterialDTO, error)
-	ListAssociationByRawMaterial(rawMaterial uuid.UUID) ([]DTOs.ProductAssocRawMaterialDTO, error)
-	ListAssociationByActivated(activated bool) ([]DTOs.ProductAssocRawMaterialDTO, error)
+	CreateAssociation(assoc []product_assoc_raw_material_DTO.ProductAssocRawMaterialDTO) error
+	UpdateAssociation(assoc []product_assoc_raw_material_DTO.ProductAssocRawMaterialDTO) error
+	DeleteAssociation(productId, materialId []string) error
+	GetAssociation(productId, materialId []string) ([]entity.ProductInterface, error)
+	GetAllAssociations() ([]entity.ProductInterface, error)
+	ListAssociationByProduct(productId []string) ([]entity.ProductInterface, error)
+	ListAssociationByRawMaterial(rawMaterialId []string) ([]entity.ProductInterface, error)
+	ListAssociationByActivated(activated bool) ([]entity.ProductInterface, error)
 }
