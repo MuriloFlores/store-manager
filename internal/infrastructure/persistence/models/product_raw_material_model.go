7cb1e86a0cbbc125241aec2d7b66cd42a7ec6a41
MuriloFlores <muriloaflore@gmail.com>
2025-03-11 22:22:04 -0300

    feat(models): create mapping for ProductRawMaterialModel to ProductRawMaterialDTO

--- internal/infrastructure/persistence/models/product_raw_material_model.go
+++ internal/infrastructure/persistence/models/product_raw_material_model.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"store-manager/internal/application/DTOs/product_assoc_raw_material_DTO"
 )
 
 type ProductRawMaterialModel struct {
@@ -9,6 +10,24 @@ type ProductRawMaterialModel struct {
 	MaterialId   uuid.UUID `gorm:"type:uuid;"`
 	QuantityUsed int       `gorm:"type:int;default:0"`
 	Activated    bool      `gorm:"type:boolean;default:false"`
+}
+
+func (p *ProductRawMaterialModel) MapProductRawMaterialAssocModelToDTO() product_assoc_raw_material_DTO.ProductAssocRawMaterialDTO {
+	return product_assoc_raw_material_DTO.ProductAssocRawMaterialDTO{
+		ProductId:    p.ProductId,
+		MaterialId:   p.MaterialId,
+		QuantityUsed: p.QuantityUsed,
+		Activated:    p.Activated,
+	}
+}
+
+func MapProductRawMaterialDTOToModel(assoc product_assoc_raw_material_DTO.ProductAssocRawMaterialDTO) ProductRawMaterialModel {
+	return ProductRawMaterialModel{
+		ProductId:    assoc.ProductId,
+		MaterialId:   assoc.MaterialId,
+		QuantityUsed: assoc.QuantityUsed,
+		Activated:    assoc.Activated,
+	}
 }
 
 func (ProductRawMaterialModel) TableName() string {
