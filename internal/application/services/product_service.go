82bfa8ce9bdac7cb422c2ca2fb923b70c7622cb8
MuriloFlores <muriloaflore@gmail.com>
2025-03-11 22:07:31 -0300

    refactor(services): update DTO definitions and adjust related structures in raw material and product services

--- internal/application/services/product_service.go
+++ internal/application/services/product_service.go
@@ -2,7 +2,7 @@ package services
 
 import (
 	"go.uber.org/zap"
-	"store-manager/internal/application/DTOs"
+	"store-manager/internal/application/DTOs/product_DTO"
 	"store-manager/internal/application/usecase/product"
 	"store-manager/internal/infrastructure/logging"
 )
@@ -16,11 +16,11 @@ type productService struct {
 }
 
 type ProductServiceInterface interface {
-	CreateProduct(input []DTOs.CreateProductDTO) ([]DTOs.ProductDTO, error)
-	FindProductById(input []DTOs.FindProductDTO) ([]DTOs.ProductDTO, error)
-	GetAllProducts() ([]DTOs.ProductDTO, error)
-	DeleteProductsByIds(input []DTOs.FindProductDTO) error
-	UpdateProducts(input []DTOs.UpdateProductDTO) ([]DTOs.ProductDTO, error)
+	CreateProduct(input []product_DTO.CreateProductDTO) ([]product_DTO.ProductDTO, error)
+	FindProductById(input []product_DTO.FindProductDTO) ([]product_DTO.ProductDTO, error)
+	GetAllProducts() ([]product_DTO.ProductDTO, error)
+	DeleteProductsByIds(input []product_DTO.FindProductDTO) error
+	UpdateProducts(input []product_DTO.UpdateProductDTO) ([]product_DTO.ProductDTO, error)
 }
 
 func NewProductService(
@@ -39,27 +39,27 @@ func NewProductService(
 	}
 }
 
-func (p *productService) CreateProduct(input []DTOs.CreateProductDTO) ([]DTOs.ProductDTO, error) {
+func (p *productService) CreateProduct(input []product_DTO.CreateProductDTO) ([]product_DTO.ProductDTO, error) {
 	logging.Info("CreateProduct Journey", zap.String("Init", "CreateProductService"))
 	return p.createProductUseCase.CreateProduct(input)
 }
 
-func (p *productService) FindProductById(input []DTOs.FindProductDTO) ([]DTOs.ProductDTO, error) {
+func (p *productService) FindProductById(input []product_DTO.FindProductDTO) ([]product_DTO.ProductDTO, error) {
 	logging.Info("FindProduct Journey", zap.String("Init", "FindProductByIdService"))
 	return p.findProductByIdUseCase.FindProductById(input)
 }
 
-func (p *productService) GetAllProducts() ([]DTOs.ProductDTO, error) {
+func (p *productService) GetAllProducts() ([]product_DTO.ProductDTO, error) {
 	logging.Info("GetAllProducts Journey", zap.String("Init", "GetAllProductsService"))
 	return p.getAllProductsUseCase.GetAllProducts()
 }
 
-func (p *productService) DeleteProductsByIds(input []DTOs.FindProductDTO) error {
+func (p *productService) DeleteProductsByIds(input []product_DTO.FindProductDTO) error {
 	logging.Info("DeleteProductsByIds Journey", zap.String("Init", "DeleteProductsByIdsService"))
 	return p.deleteProductByIdUseCase.DeleteProductById(input)
 }
 
-func (p *productService) UpdateProducts(input []DTOs.UpdateProductDTO) ([]DTOs.ProductDTO, error) {
+func (p *productService) UpdateProducts(input []product_DTO.UpdateProductDTO) ([]product_DTO.ProductDTO, error) {
 	logging.Info("UpdateProducts Journey", zap.String("Init", "UpdateProductsService"))
 	return p.updateProductsUseCase.UpdateProduct(input)
 }
--- internal/application/services/raw_material_service.go
+++ internal/application/services/raw_material_service.go
@@ -2,25 +2,27 @@ package services
 
 import (
 	"go.uber.org/zap"
-	"store-manager/internal/application/DTOs"
+	"store-manager/internal/application/DTOs/raw_material_DTO"
 	"store-manager/internal/application/usecase/raw_material"
 	"store-manager/internal/infrastructure/logging"
 )
 
 type rawMaterialService struct {
-	createRawMaterialUseCase  raw_material.CreateRawMaterialUseCaseInterface
-	findRawMaterialUseCase    raw_material.FindRawMaterialByIdUseCaseInterface
-	getAllRawMaterialsUseCase raw_material.GetAllRawMaterialsUseCaseInterface
-	deleteRawMaterialUseCase  raw_material.DeleteRawMaterialUseCaseInterface
-	updateRawMaterialUseCase  raw_material.UpdateRawMaterialUseCaseInterface
+	createRawMaterialUseCase         raw_material.CreateRawMaterialUseCaseInterface
+	findRawMaterialUseCase           raw_material.FindRawMaterialByIdUseCaseInterface
+	getAllRawMaterialsUseCase        raw_material.GetAllRawMaterialsUseCaseInterface
+	deleteRawMaterialUseCase         raw_material.DeleteRawMaterialUseCaseInterface
+	updateRawMaterialUseCase         raw_material.UpdateRawMaterialUseCaseInterface
+	getAllRawMaterialsByLimitUseCase raw_material.GetAllRawMaterialsByLimitUseCaseInterface
 }
 
 type RawMaterialServiceInterface interface {
-	CreateRawMaterial(input []DTOs.CreateRawMaterialDTO) ([]DTOs.RawMaterialDTO, error)
-	FindRawMaterialById(input []DTOs.FindRawMaterialDTO) ([]DTOs.RawMaterialDTO, error)
-	GetAllRawMaterials() ([]DTOs.RawMaterialDTO, error)
-	DeleteRawMaterial(dtos []DTOs.FindRawMaterialDTO) error
-	UpdateRawMaterial(input []DTOs.RawMaterialDTO) ([]DTOs.RawMaterialDTO, error)
+	CreateRawMaterial(input []raw_material_DTO.CreateRawMaterialDTO) ([]raw_material_DTO.RawMaterialDTO, error)
+	FindRawMaterialById(input []raw_material_DTO.FindRawMaterialDTO) ([]raw_material_DTO.RawMaterialDTO, error)
+	GetAllRawMaterials() ([]raw_material_DTO.RawMaterialDTO, error)
+	GetAllRawMaterialsByLimitRisk() ([]raw_material_DTO.RawMaterialDTO, error)
+	DeleteRawMaterial(dtos []raw_material_DTO.FindRawMaterialDTO) error
+	UpdateRawMaterial(input []raw_material_DTO.RawMaterialDTO) ([]raw_material_DTO.RawMaterialDTO, error)
 }
 
 func NewRawMaterialService(
@@ -29,37 +31,44 @@ func NewRawMaterialService(
 	getAllRawMaterialsUseCase raw_material.GetAllRawMaterialsUseCaseInterface,
 	deleteRawMaterialUseCase raw_material.DeleteRawMaterialUseCaseInterface,
 	updateRawMaterialUseCase raw_material.UpdateRawMaterialUseCaseInterface,
+	getAllRawMaterialByLimitUseCase raw_material.GetAllRawMaterialsByLimitUseCaseInterface,
 ) RawMaterialServiceInterface {
 	return &rawMaterialService{
-		createRawMaterialUseCase:  createRawMaterialUseCase,
-		findRawMaterialUseCase:    findRawMaterialUseCase,
-		getAllRawMaterialsUseCase: getAllRawMaterialsUseCase,
-		deleteRawMaterialUseCase:  deleteRawMaterialUseCase,
-		updateRawMaterialUseCase:  updateRawMaterialUseCase,
+		createRawMaterialUseCase:         createRawMaterialUseCase,
+		findRawMaterialUseCase:           findRawMaterialUseCase,
+		getAllRawMaterialsUseCase:        getAllRawMaterialsUseCase,
+		deleteRawMaterialUseCase:         deleteRawMaterialUseCase,
+		updateRawMaterialUseCase:         updateRawMaterialUseCase,
+		getAllRawMaterialsByLimitUseCase: getAllRawMaterialByLimitUseCase,
 	}
 }
 
-func (r *rawMaterialService) CreateRawMaterial(input []DTOs.CreateRawMaterialDTO) ([]DTOs.RawMaterialDTO, error) {
+func (r *rawMaterialService) CreateRawMaterial(input []raw_material_DTO.CreateRawMaterialDTO) ([]raw_material_DTO.RawMaterialDTO, error) {
 	logging.Info("CreateRawMaterial Journey", zap.String("Init", "CreateRawMaterialService"))
 	return r.createRawMaterialUseCase.CreateRawMaterial(input)
 }
 
-func (r *rawMaterialService) FindRawMaterialById(input []DTOs.FindRawMaterialDTO) ([]DTOs.RawMaterialDTO, error) {
+func (r *rawMaterialService) FindRawMaterialById(input []raw_material_DTO.FindRawMaterialDTO) ([]raw_material_DTO.RawMaterialDTO, error) {
 	logging.Info("FindRawMaterialById Journey", zap.String("Init", "FindRawMaterialByIdService"))
 	return r.findRawMaterialUseCase.FindRawMaterialById(input)
 }
 
-func (r *rawMaterialService) GetAllRawMaterials() ([]DTOs.RawMaterialDTO, error) {
+func (r *rawMaterialService) GetAllRawMaterials() ([]raw_material_DTO.RawMaterialDTO, error) {
 	logging.Info("GetAllRawMaterials Journey", zap.String("Init", "GetAllRawMaterialsService"))
 	return r.getAllRawMaterialsUseCase.GetAllRawMaterials()
 }
 
-func (r *rawMaterialService) DeleteRawMaterial(dtos []DTOs.FindRawMaterialDTO) error {
+func (r *rawMaterialService) GetAllRawMaterialsByLimitRisk() ([]raw_material_DTO.RawMaterialDTO, error) {
+	logging.Info("GetAllRawMaterialsByLimit Journey", zap.String("Init", "GetAllRawMaterialsByLimitRisk"))
+	return r.getAllRawMaterialsByLimitUseCase.GetAllRawMaterialsByLimit()
+}
+
+func (r *rawMaterialService) DeleteRawMaterial(dtos []raw_material_DTO.FindRawMaterialDTO) error {
 	logging.Info("DeleteRawMaterial Journey", zap.String("Init", "DeleteRawMaterialService"))
 	return r.deleteRawMaterialUseCase.DeleteRawMaterial(dtos)
 }
 
-func (r *rawMaterialService) UpdateRawMaterial(input []DTOs.RawMaterialDTO) ([]DTOs.RawMaterialDTO, error) {
+func (r *rawMaterialService) UpdateRawMaterial(input []raw_material_DTO.RawMaterialDTO) ([]raw_material_DTO.RawMaterialDTO, error) {
 	logging.Info("UpdateRawMaterial Journey", zap.String("Init", "UpdateRawMaterialService"))
 	return r.updateRawMaterialUseCase.UpdateRawMaterial(input)
 }
