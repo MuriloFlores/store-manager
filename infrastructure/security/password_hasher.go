d6e1a938514a3de15f106f699d083f9f4e82abf6
MuriloFlores <muriloaflore@gmail.com>
2025-06-07 23:04:54 -0300

    feat(hasher): implement password hasher and hash compare

--- infrastructure/security/password_hasher.go
+++ infrastructure/security/password_hasher.go
@@ -1,1 +1,27 @@
 package security
+
+import (
+	"github.com/muriloFlores/StoreManager/internal/core/ports"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type bcryptHasher struct{}
+
+func NewPasswordHasher() ports.PasswordHasher {
+	return &bcryptHasher{}
+}
+
+func (h *bcryptHasher) Hash(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func (h *bcryptHasher) Compare(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	return err == nil
+}
