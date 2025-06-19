24d318fe1716c4ba1a1894ba7b3fcf8cb57b1f2b
MuriloFlores <muriloaflore@gmail.com>
2025-06-18 22:58:52 -0300

    feat(domain): create facade for user use cases

--- internal/core/use_case/user/user_use_cases.go
+++ internal/core/use_case/user/user_use_cases.go
@@ -1,21 +1,21 @@
 package user
 
-import "github.com/muriloFlores/StoreManager/internal/core/ports"
+import (
+	"github.com/muriloFlores/StoreManager/internal/core/ports"
+)
 
 type UserUseCases struct {
 	Create *CreateUserUseCase
 	Find   *FindUserUseCase
 	Update *UpdateUserUseCase
 	Delete *DeleteUserUseCase
-	Login  *LoginUserUseCase
 }
 
-func NewUserUseCases(userRepo ports.UserRepository, hasher ports.PasswordHasher, generator ports.IDGenerator, tokenManager ports.TokenManager) *UserUseCases {
+func NewUserUseCases(userRepo ports.UserRepository, hasher ports.PasswordHasher, generator ports.IDGenerator) *UserUseCases {
 	return &UserUseCases{
 		Create: NewCreateUserUseCase(userRepo, hasher, generator),
 		Find:   NewFindUserUseCase(userRepo),
 		Update: NewUpdateUserUseCase(userRepo, hasher),
 		Delete: NewDeleteUserUseCase(userRepo),
-		Login:  NewLoginUserUseCase(userRepo, hasher, tokenManager),
 	}
 }
