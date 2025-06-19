bab0b1deb7644b69c0d2416bc8d0e5d0ecc0bb60
MuriloFlores <muriloaflore@gmail.com>
2025-06-18 22:57:27 -0300

    feat(domain): create CRUD for user

--- internal/core/use_case/user/update_user.go
+++ internal/core/use_case/user/update_user.go
@@ -1,1 +1,99 @@
 package user
+
+import (
+	"context"
+	"github.com/muriloFlores/StoreManager/internal/core/domain"
+	"github.com/muriloFlores/StoreManager/internal/core/ports"
+	"github.com/muriloFlores/StoreManager/internal/core/value_objects"
+)
+
+type UpdateUserUseCase struct {
+	userRepository ports.UserRepository
+	hasher         ports.PasswordHasher
+}
+
+type UpdateUserParams struct {
+	Name *string
+	Role *string
+}
+
+func NewUpdateUserUseCase(userRepository ports.UserRepository, hasher ports.PasswordHasher) *UpdateUserUseCase {
+	return &UpdateUserUseCase{
+		userRepository: userRepository,
+		hasher:         hasher,
+	}
+}
+
+func (uc *UpdateUserUseCase) Execute(ctx context.Context, actor *domain.Identity, targetUserID string, params UpdateUserParams) (*domain.User, error) {
+	targetUser, err := uc.userRepository.FindByID(ctx, targetUserID)
+	if err != nil {
+		return nil, err
+	}
+
+	if params.Name != nil {
+		if actor.UserID != targetUserID && actor.Role != value_objects.Admin {
+			return nil, &domain.ErrForbidden{Action: "attempt to update another user's name"}
+		}
+
+		if err = targetUser.ChangeName(*params.Name); err != nil {
+			return nil, err
+		}
+	}
+
+	if params.Role != nil {
+		newRole := value_objects.Role(*params.Role)
+
+		if err = uc.canChangeRole(actor, targetUser, newRole); err != nil {
+			return nil, err
+		}
+
+		if err = targetUser.ChangeRole(newRole); err != nil {
+			return nil, err
+		}
+	}
+
+	if err = uc.userRepository.Update(ctx, targetUser); err != nil {
+		return nil, err
+	}
+
+	return targetUser, nil
+}
+
+func (uc *UpdateUserUseCase) canChangeRole(actor *domain.Identity, targetUser *domain.User, newRole value_objects.Role) error {
+	actorRole := actor.Role
+	targetRole := targetUser.Role()
+
+	switch actorRole {
+	case value_objects.Admin:
+		if actor.UserID == targetUser.ID() && newRole == value_objects.Admin {
+			count, err := uc.userRepository.CountAdmins(context.Background())
+			if err != nil {
+				return err
+			}
+
+			if count <= 1 {
+				return &domain.ErrForbidden{Action: "attempt to change an admin"}
+			}
+
+		}
+
+		return nil
+
+	case value_objects.Manager:
+		if targetRole == value_objects.Manager || targetRole == value_objects.Admin {
+			return &domain.ErrForbidden{Action: "alterar o cargo de um manager ou admin"}
+		}
+
+		if targetRole == value_objects.Client {
+			return &domain.ErrForbidden{Action: "alterar o cargo de um cliente"}
+		}
+
+		if newRole == value_objects.Admin || newRole == value_objects.Manager {
+			return &domain.ErrForbidden{Action: "promover um usuário para manager ou admin"}
+		}
+
+		return nil
+	}
+
+	return &domain.ErrForbidden{Action: "attempt to update another user's role"}
+}
