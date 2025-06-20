69f13f8da1c788b449107069fc771b98dc64dc75
MuriloFlores <muriloaflore@gmail.com>
2025-06-20 18:10:18 -0300

    refactor(logging): adding logger process

--- infrastructure/db/postgres_repository/action_token_repository.go
+++ infrastructure/db/postgres_repository/action_token_repository.go
@@ -12,14 +12,17 @@ import (
 )
 
 type ActionTokenRepository struct {
-	db *pgxpool.Pool
+	db     *pgxpool.Pool
+	logger ports.Logger
 }
 
-func NewActionTokenRepository(db *pgxpool.Pool) ports.ActionTokenRepository {
-	return &ActionTokenRepository{db: db}
+func NewActionTokenRepository(db *pgxpool.Pool, logger ports.Logger) ports.ActionTokenRepository {
+	return &ActionTokenRepository{db: db, logger: logger}
 }
 
 func (r *ActionTokenRepository) Create(ctx context.Context, token *domain.ActionToken) error {
+	r.logger.InfoLevel("Invoking Create for token", map[string]interface{}{"token": token.Token, "type": token.Type})
+
 	query := `INSERT INTO action_tokens (token, user_id, type, payload,  expires_at) VALUES ($1, $2, $3, $4, $5)`
 
 	_, err := r.db.Exec(ctx, query,
@@ -31,22 +34,31 @@ func (r *ActionTokenRepository) Create(ctx context.Context, token *domain.Action
 	)
 
 	if err != nil {
+		r.logger.ErrorLevel("Error creating token", err, map[string]interface{}{"token": token.Token, "type": token.Type})
 		return fmt.Errorf("error while reset token: %w", err)
 	}
+
+	r.logger.InfoLevel("Token created successfully", map[string]interface{}{"token": token.Token, "type": token.Type})
 
 	return nil
 }
 
 func (r *ActionTokenRepository) FindAndConsume(ctx context.Context, tokenString string, tokenType domain.ActionType) (*domain.ActionToken, error) {
+	r.logger.InfoLevel("Invoking FindAndConsume for token", map[string]interface{}{"token": tokenString, "type": tokenType})
+
 	query := `DELETE FROM action_tokens WHERE token = $1 AND type = $2 RETURNING user_id, type, payload, expires_at`
 
 	var userID, payload string
 	var foundType domain.ActionType
 	var expiresAt time.Time
 
-	err := r.db.QueryRow(ctx, query, tokenString).Scan(&userID, &foundType, &payload, &expiresAt)
+	err := r.db.QueryRow(ctx, query, tokenString, tokenType).Scan(&userID, &foundType, &payload, &expiresAt)
 	if err != nil {
+		r.logger.ErrorLevel("Error consuming token", err, map[string]interface{}{"token": tokenString, "type": tokenType})
+
 		if errors.Is(err, pgx.ErrNoRows) {
+			r.logger.ErrorLevel("Token not found", err, map[string]interface{}{"token": tokenString, "type": tokenType})
+
 			return nil, &domain.ErrInvalidToken{Reason: "token not found"}
 		}
 
@@ -54,6 +66,7 @@ func (r *ActionTokenRepository) FindAndConsume(ctx context.Context, tokenString 
 	}
 
 	if time.Now().After(expiresAt) {
+		r.logger.ErrorLevel("Token expired", nil, map[string]interface{}{"token": tokenString, "type": tokenType})
 		return nil, &domain.ErrInvalidToken{Reason: "token expired"}
 	}
 
--- infrastructure/web/http/auth_handler.go
+++ infrastructure/web/http/auth_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/muriloFlores/StoreManager/infrastructure/validation"
 	"github.com/muriloFlores/StoreManager/infrastructure/web"
+	"github.com/muriloFlores/StoreManager/internal/core/ports"
 
 	dto "github.com/muriloFlores/StoreManager/infrastructure/web/DTO/authDTO"
 	"github.com/muriloFlores/StoreManager/infrastructure/web/middleware"
@@ -15,24 +16,29 @@ import (
 
 type AuthHandler struct {
 	useCases *auth.AuthUseCases
+	logger   ports.Logger
 }
 
-func NewAuthHandler(useCases *auth.AuthUseCases) *AuthHandler {
+func NewAuthHandler(useCases *auth.AuthUseCases, logger ports.Logger) *AuthHandler {
 	return &AuthHandler{
 		useCases: useCases,
+		logger:   logger,
 	}
 }
 
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	h.logger.InfoLevel("Login handler invoked", nil)
 	var req dto.LoginRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		h.logger.ErrorLevel("Failed to decode request body", err)
 		restErr := web_errors.NewBadRequestError("invalid body request")
 		restErr.Send(w)
 		return
 	}
 
 	if err := validation.Validate.Struct(req); err != nil {
+		h.logger.ErrorLevel("Validation error", err, map[string]interface{}{"email": req.Email, "password": req.Password})
 		restErr := validation.TranslateError(err)
 		restErr.Send(w)
 		return
@@ -40,28 +46,36 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 
 	token, err := h.useCases.Login.Execute(r.Context(), req.Email, req.Password)
 	if err != nil {
+		h.logger.ErrorLevel("Login use case error", err, map[string]interface{}{"email": req.Email})
 		web.HandleError(w, err)
 		return
 	}
 
+	h.logger.InfoLevel("User logged in successfully", map[string]interface{}{"email": req.Email})
 	respondWithJSON(w, http.StatusOK, dto.LoginResponse{Token: token})
 }
 
 func (h *AuthHandler) ChangePassword(w http.ResponseWriter, r *http.Request) {
+	h.logger.InfoLevel("ChangePassword handler invoked", nil)
+
 	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
 	if !ok {
+		h.logger.ErrorLevel("Failed to retrieve user identity from context", nil)
 		restErr := web_errors.NewInternalServerError("unable to retrieve user identity")
 		restErr.Send(w)
 		return
 	}
 
 	var req dto.ChangePasswordRequest
+
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		h.logger.ErrorLevel("Failed to decode request body", err)
 		web_errors.NewBadRequestError("invalid json body").Send(w)
 		return
 	}
 
 	if err := validation.Validate.Struct(req); err != nil {
+		h.logger.ErrorLevel("Validation error", err, map[string]interface{}{"old_password": req.OldPassword, "new_password": req.NewPassword})
 		restErr := validation.TranslateError(err)
 		restErr.Send(w)
 		return
@@ -69,9 +83,59 @@ func (h *AuthHandler) ChangePassword(w http.ResponseWriter, r *http.Request) {
 
 	err := h.useCases.ChangePassword.Execute(r.Context(), actorIdentity, req.OldPassword, req.NewPassword)
 	if err != nil {
+		h.logger.ErrorLevel("ChangePassword use case error", err, map[string]interface{}{"user_id": actorIdentity.UserID})
 		web.HandleError(w, err)
 		return
 	}
 
+	h.logger.InfoLevel("Password changed successfully", map[string]interface{}{"user_id": actorIdentity.UserID})
 	respondWithJSON(w, http.StatusNoContent, nil)
+}
+
+func (h *AuthHandler) ConfirmEmail(w http.ResponseWriter, r *http.Request) {
+	h.logger.InfoLevel("ConfirmEmail handler invoked", nil)
+
+	token := r.URL.Query().Get("token")
+
+	if token == "" {
+		h.logger.ErrorLevel("Token is missing in the request", nil)
+		restErr := web_errors.NewBadRequestError("invalid token")
+		restErr.Send(w)
+		return
+	}
+
+	err := h.useCases.ConfirmUserEmailUseCase.Execute(r.Context(), token)
+	if err != nil {
+		h.logger.ErrorLevel("ConfirmUserEmail use case error", err, map[string]interface{}{"token": token})
+		web.HandleError(w, err)
+		return
+	}
+
+	h.logger.InfoLevel("Email confirmed successfully", map[string]interface{}{"token": token})
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"message": "Email verificado com sucesso!"})
+}
+
+func (h *AuthHandler) ConfirmAccount(w http.ResponseWriter, r *http.Request) {
+	h.logger.InfoLevel("ConfirmAccount handler invoked", nil)
+
+	token := r.URL.Query().Get("token")
+
+	if token == "" {
+		h.logger.ErrorLevel("Token is missing in the request", nil)
+		restErr := web_errors.NewBadRequestError("invalid token")
+		restErr.Send(w)
+		return
+	}
+
+	err := h.useCases.ConfirmAccountUserUseCase.Execute(r.Context(), token)
+	if err != nil {
+		h.logger.ErrorLevel("ConfirmAccount use case error", err, map[string]interface{}{"token": token})
+		web.HandleError(w, err)
+		return
+	}
+
+	h.logger.InfoLevel("Account confirmed successfully", map[string]interface{}{"token": token})
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"message": "Conta verificada com sucesso!"})
 }
