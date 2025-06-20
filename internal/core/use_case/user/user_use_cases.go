0d60ff47919d9e611f6118ad89bab6adc41ea095
MuriloFlores <muriloaflore@gmail.com>
2025-06-19 21:38:35 -0300

    feat(logger): improved log system and aplying it in create user and worker process

--- internal/core/use_case/user/user_use_cases.go
+++ internal/core/use_case/user/user_use_cases.go
@@ -18,9 +18,10 @@ func NewUserUseCases(
 	tokenGenerator ports.SecureTokenGenerator,
 	taskEnqueuer ports.TaskEnqueuer,
 	tokenRepo ports.ActionTokenRepository,
+	logger ports.Logger,
 ) *UserUseCases {
 	return &UserUseCases{
-		Create: NewCreateUserUseCase(userRepo, hasher, generator, tokenGenerator, taskEnqueuer, tokenRepo),
+		Create: NewCreateUserUseCase(userRepo, hasher, generator, tokenGenerator, taskEnqueuer, tokenRepo, logger),
 		Find:   NewFindUserUseCase(userRepo),
 		Update: NewUpdateUserUseCase(userRepo, hasher),
 		Delete: NewDeleteUserUseCase(userRepo),
