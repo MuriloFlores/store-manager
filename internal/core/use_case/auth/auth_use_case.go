8d070d5b095af5047bd151a291ae3e457f3fb67a
MuriloFlores <muriloaflore@gmail.com>
2025-06-21 16:00:52 -0300

    refactor(auth): refactor account validation process

--- internal/core/use_case/auth/auth_use_case.go
+++ internal/core/use_case/auth/auth_use_case.go
@@ -3,14 +3,15 @@ package auth
 import "github.com/muriloFlores/StoreManager/internal/core/ports"
 
 type AuthUseCases struct {
-	Login                     *LoginUserUseCase
-	ChangePassword            *ChangePasswordUseCase
-	ConfirmPasswordReset      *ConfirmPasswordResetUseCase
-	ConfirmUserEmailUseCase   *ConfirmUserEmailUseCase
-	ConfirmEmailChangeUseCase *ConfirmEmailChangeUseCase
-	RequestEmailChange        *RequestEmailChangeUseCase
-	RequestPasswordReset      *RequestPasswordResetUseCase
-	ConfirmAccountUserUseCase *ConfirmAccountUserUseCase
+	Login                           *LoginUserUseCase
+	ChangePassword                  *ChangePasswordUseCase
+	ConfirmPasswordReset            *ConfirmPasswordResetUseCase
+	ConfirmUserEmailUseCase         *ConfirmUserEmailUseCase
+	ConfirmEmailChangeUseCase       *ConfirmEmailChangeUseCase
+	RequestEmailChange              *RequestEmailChangeUseCase
+	RequestPasswordReset            *RequestPasswordResetUseCase
+	ConfirmAccountUserUseCase       *ConfirmAccountUserUseCase
+	RequestAccountValidationUseCase *RequestAccountValidationUseCase
 }
 
 func NewAuthUseCases(
@@ -23,13 +24,14 @@ func NewAuthUseCases(
 	logger ports.Logger,
 ) *AuthUseCases {
 	return &AuthUseCases{
-		Login:                     NewLoginUserUseCase(userRepo, hasher, manager, logger),
-		ChangePassword:            NewChangePasswordUseCase(userRepo, hasher),
-		RequestPasswordReset:      NewRequestPasswordResetUseCase(userRepo, tokenRepo, tokenGen, taskEnqueuer),
-		ConfirmPasswordReset:      NewConfirmPasswordResetUseCase(userRepo, tokenRepo, hasher),
-		RequestEmailChange:        NewRequestEmailChangeUseCase(userRepo, tokenRepo, tokenGen, taskEnqueuer, hasher),
-		ConfirmEmailChangeUseCase: NewConfirmEmailChangeUseCase(userRepo, tokenRepo),
-		ConfirmUserEmailUseCase:   NewConfirmUserEmailUseCase(userRepo, tokenRepo, logger),
-		ConfirmAccountUserUseCase: NewConfirmAccountUserUseCase(userRepo, tokenRepo, logger),
+		Login:                           NewLoginUserUseCase(userRepo, hasher, manager, logger),
+		ChangePassword:                  NewChangePasswordUseCase(userRepo, hasher),
+		RequestPasswordReset:            NewRequestPasswordResetUseCase(userRepo, tokenRepo, tokenGen, taskEnqueuer),
+		ConfirmPasswordReset:            NewConfirmPasswordResetUseCase(userRepo, tokenRepo, hasher),
+		RequestEmailChange:              NewRequestEmailChangeUseCase(userRepo, tokenRepo, tokenGen, taskEnqueuer, hasher),
+		ConfirmEmailChangeUseCase:       NewConfirmEmailChangeUseCase(userRepo, tokenRepo),
+		ConfirmUserEmailUseCase:         NewConfirmUserEmailUseCase(userRepo, tokenRepo, logger),
+		ConfirmAccountUserUseCase:       NewConfirmAccountUserUseCase(userRepo, tokenRepo, logger),
+		RequestAccountValidationUseCase: NewRequestAccountValidationUseCase(userRepo, tokenRepo, tokenGen, taskEnqueuer, logger),
 	}
 }
---
+++ internal/core/use_case/auth/request_account_validation.go
@@ -0,0 +1,75 @@
+package auth
+
+import (
+	"context"
+	"github.com/muriloFlores/StoreManager/internal/core/domain"
+	"github.com/muriloFlores/StoreManager/internal/core/domain/jobs"
+	"github.com/muriloFlores/StoreManager/internal/core/ports"
+	"time"
+)
+
+type RequestAccountValidationUseCase struct {
+	userRepo       ports.UserRepository
+	tokenRepo      ports.ActionTokenRepository
+	tokenGenerator ports.SecureTokenGenerator
+	taskEnqueuer   ports.TaskEnqueuer
+	logger         ports.Logger
+}
+
+func NewRequestAccountValidationUseCase(
+	userRepo ports.UserRepository,
+	tokenRepo ports.ActionTokenRepository,
+	tokenGenerator ports.SecureTokenGenerator,
+	taskEnqueuer ports.TaskEnqueuer,
+	logger ports.Logger,
+) *RequestAccountValidationUseCase {
+	return &RequestAccountValidationUseCase{
+		userRepo:       userRepo,
+		tokenRepo:      tokenRepo,
+		tokenGenerator: tokenGenerator,
+		taskEnqueuer:   taskEnqueuer,
+		logger:         logger,
+	}
+}
+
+func (uc *RequestAccountValidationUseCase) Execute(ctx context.Context, userID string) error {
+	uc.logger.InfoLevel("Request account validation use case started", map[string]interface{}{"user_id": userID})
+
+	user, err := uc.userRepo.FindByID(ctx, userID)
+	if err != nil {
+		uc.logger.ErrorLevel("Error finding user by ID", err, map[string]interface{}{"user_id": userID})
+		return err
+	}
+
+	verificationTokenString, err := uc.tokenGenerator.Generate()
+	if err != nil {
+		uc.logger.ErrorLevel("Error generating verification token", err, nil)
+		return err
+	}
+
+	actionToken := &domain.ActionToken{
+		Token:     verificationTokenString,
+		UserID:    userID,
+		Type:      domain.AccountVerification,
+		ExpiresAt: time.Now().Add(time.Minute * 30),
+	}
+
+	if err = uc.tokenRepo.Create(ctx, actionToken); err != nil {
+		uc.logger.ErrorLevel("Error creating action token", err, map[string]interface{}{"user_id": userID})
+		return err
+	}
+
+	jobData := &jobs.AccountVerificationJobData{
+		UserName:         user.Name(),
+		ToEmail:          user.Email(),
+		VerificationLink: "https://muriloflores.xyz/auth/verify-account?token=" + verificationTokenString,
+	}
+
+	if err = uc.taskEnqueuer.EnqueueAccountVerification(jobData); err != nil {
+		uc.logger.ErrorLevel("Error enqueuing account verification job", err, map[string]interface{}{"user_id": userID})
+		return err
+	}
+
+	uc.logger.InfoLevel("Account validation request processed successfully", map[string]interface{}{"user_id": userID, "user_email": user.Email()})
+	return nil
+}
