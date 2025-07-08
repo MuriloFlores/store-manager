09b4b7bf7d0a0adc2fbee8ed77672f4b505932af
MuriloFlores <muriloaflore@gmail.com>
2025-07-08 16:25:44 -0300

    feat(http): create item http handler

---
+++ infrastructure/web/http/item_handler.go
@@ -0,0 +1,264 @@
+package http
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/muriloFlores/StoreManager/infrastructure/validation"
+	"github.com/muriloFlores/StoreManager/infrastructure/web"
+	"github.com/muriloFlores/StoreManager/infrastructure/web/DTO/item_dto"
+	"github.com/muriloFlores/StoreManager/infrastructure/web/DTO/pagination_dto"
+	"github.com/muriloFlores/StoreManager/infrastructure/web/middleware"
+	"github.com/muriloFlores/StoreManager/infrastructure/web/web_errors"
+	"github.com/muriloFlores/StoreManager/internal/core/domain"
+	"github.com/muriloFlores/StoreManager/internal/core/domain/item"
+	"github.com/muriloFlores/StoreManager/internal/core/ports"
+	"github.com/muriloFlores/StoreManager/internal/core/use_case/items"
+	"github.com/muriloFlores/StoreManager/internal/core/value_objects"
+	"net/http"
+)
+
+type ItemHandler struct {
+	useCase items.ItemsUseCases
+	logger  ports.Logger
+}
+
+func NewItemHandler(useCase items.ItemsUseCases, logger ports.Logger) *ItemHandler {
+	return &ItemHandler{
+		useCase: useCase,
+		logger:  logger,
+	}
+}
+
+func (h *ItemHandler) ListPublicItems(w http.ResponseWriter, r *http.Request) {
+	params := pagination_dto.ParsePagination(r)
+
+	paginatedResult, err := h.useCase.List.ListPublic(r.Context(), params)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	publicResponses := make([]item_dto.ClientItemResponse, 0, len(paginatedResult.Data))
+
+	for _, itemData := range paginatedResult.Data {
+		publicResponses = append(publicResponses, item_dto.ToClientItemResponse(itemData))
+	}
+
+	response := pagination_dto.PaginatedResponse[item_dto.ClientItemResponse]{
+		Data:       publicResponses,
+		Pagination: pagination_dto.ToPaginationInfoResponse(paginatedResult.Pagination),
+	}
+
+	respondWithJSON(w, http.StatusOK, response)
+
+}
+
+func (h *ItemHandler) ListInternalItems(w http.ResponseWriter, r *http.Request) {
+	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
+	if !ok {
+		web_errors.NewInternalServerError("user identity not found in context").Send(w)
+		return
+	}
+
+	params := pagination_dto.ParsePagination(r)
+
+	paginatedResult, err := h.useCase.List.ListInternal(r.Context(), actorIdentity, params)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	actorRole := actorIdentity.Role
+
+	if actorRole == value_objects.Admin || actorRole == value_objects.Manager || actorRole == value_objects.StockPerson {
+		internalResponse := make([]item_dto.InternalItemResponse, 0, len(paginatedResult.Data))
+
+		for _, itemData := range paginatedResult.Data {
+
+			internalResponse = append(internalResponse, item_dto.ToInternalItemResponse(itemData))
+		}
+
+		response := pagination_dto.PaginatedResponse[item_dto.InternalItemResponse]{
+			Data:       internalResponse,
+			Pagination: pagination_dto.ToPaginationInfoResponse(paginatedResult.Pagination),
+		}
+
+		respondWithJSON(w, http.StatusOK, response)
+	} else {
+
+		web_errors.NewForbiddenError("You do not have permission to access this route").Send(w)
+	}
+
+}
+
+func (h *ItemHandler) CreateItem(w http.ResponseWriter, r *http.Request) {
+	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
+	if !ok {
+		web_errors.NewInternalServerError("user identity not found in context").Send(w)
+		return
+	}
+
+	var req item_dto.CreateItemRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		web_errors.NewBadRequestError("invalid json body").Send(w)
+		return
+	}
+
+	if err := validation.Validate.Struct(&req); err != nil {
+		restErr := validation.TranslateError(err)
+		restErr.Send(w)
+		return
+	}
+
+	itemParam := items.CreateItemParams{
+		Name:              req.Name,
+		Description:       req.Description,
+		SKU:               req.SKU,
+		ItemType:          item.ItemType(req.ItemType),
+		IsActive:          req.Active,
+		CanBeSold:         req.CanBeSold,
+		PriceSaleInCents:  req.PriceInCents,
+		PriceCostInCents:  req.PriceCostInCents,
+		StockQuantity:     req.StockQuantity,
+		UnitOfMeasure:     req.UnitOfMeasure,
+		MinimumStockLevel: req.MinimumStockLevel,
+	}
+
+	createItem, err := h.useCase.Create.Execute(r.Context(), itemParam, actorIdentity)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	response := item_dto.ToInternalItemResponse(createItem)
+
+	respondWithJSON(w, http.StatusCreated, response)
+}
+
+func (h *ItemHandler) DeleteItem(w http.ResponseWriter, r *http.Request) {
+	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
+	if !ok {
+		web_errors.NewInternalServerError("user identity not found in context").Send(w)
+		return
+	}
+
+	vars := mux.Vars(r)
+	targetID, ok := vars["id"]
+	if !ok {
+		web_errors.NewBadRequestError("item ID not provided").Send(w)
+		return
+	}
+
+	if err := h.useCase.Delete.Execute(r.Context(), actorIdentity, targetID); err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusNoContent, nil)
+}
+
+func (h *ItemHandler) FindItemByID(w http.ResponseWriter, r *http.Request) {
+	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
+	if !ok {
+		web_errors.NewInternalServerError("user identity not found in context").Send(w)
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	itemID, ok := vars["id"]
+	if !ok {
+		web_errors.NewBadRequestError("item ID not provided").Send(w)
+		return
+	}
+
+	itemInfo, err := h.useCase.Find.FindByID(r.Context(), itemID, actorIdentity)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	response := item_dto.ToInternalItemResponse(itemInfo)
+
+	respondWithJSON(w, http.StatusOK, response)
+}
+
+func (h *ItemHandler) FindItemBySKU(w http.ResponseWriter, r *http.Request) {
+	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
+	if !ok {
+		web_errors.NewInternalServerError("user identity not found in context").Send(w)
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	itemSKU, ok := vars["id"]
+	if !ok {
+		web_errors.NewBadRequestError("item SKU not provided").Send(w)
+		return
+	}
+
+	itemInfo, err := h.useCase.Find.FindBySKU(r.Context(), itemSKU, actorIdentity)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	response := item_dto.ToInternalItemResponse(itemInfo)
+
+	respondWithJSON(w, http.StatusOK, response)
+}
+
+func (h *ItemHandler) FindByName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	itemName, ok := vars["name"]
+	if !ok {
+		web_errors.NewBadRequestError("item name not provided").Send(w)
+		return
+	}
+
+	itemInfo, err := h.useCase.Find.FindByName(r.Context(), itemName)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	response := item_dto.ToClientItemResponse(itemInfo)
+
+	respondWithJSON(w, http.StatusOK, response)
+}
+
+func (h *ItemHandler) UpdateItem(w http.ResponseWriter, r *http.Request) {
+	actorIdentity, ok := r.Context().Value(middleware.UserIdentityKey).(*domain.Identity)
+	if !ok {
+		web_errors.NewInternalServerError("user service not found in context").Send(w)
+		return
+	}
+
+	vars := mux.Vars(r)
+	targetID, ok := vars["id"]
+
+	var req item_dto.UpdateItemRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		web_errors.NewInternalServerError("invalid request body").Send(w)
+		return
+	}
+
+	if err := validation.Validate.Struct(&req); err != nil {
+		validation.TranslateError(err).Send(w)
+		return
+	}
+
+	params := items.UpdateItemParams{}
+
+	updatedItem, err := h.useCase.Update.Execute(r.Context(), actorIdentity, targetID, params)
+	if err != nil {
+		web.HandleError(w, err)
+		return
+	}
+
+	response := item_dto.ToInternalItemResponse(updatedItem)
+	respondWithJSON(w, http.StatusOK, response)
+}
