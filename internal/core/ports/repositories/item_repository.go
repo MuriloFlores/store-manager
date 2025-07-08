0bdd69f22cf4560619ff68c50c40890df90f8e13
MuriloFlores <muriloaflore@gmail.com>
2025-07-08 16:40:50 -0300

    feat(domain): add find by name and delete in interface

--- internal/core/ports/repositories/item_repository.go
+++ internal/core/ports/repositories/item_repository.go
@@ -10,6 +10,8 @@ type ItemRepository interface {
 	Save(ctx context.Context, item *item.Item) error
 	FindByID(ctx context.Context, id string) (*item.Item, error)
 	FindBySKU(ctx context.Context, sku string) (*item.Item, error)
+	FindByName(ctx context.Context, name string) (*item.Item, error)
 	Update(ctx context.Context, item *item.Item) error
+	Delete(ctx context.Context, itemID string) error
 	List(ctx context.Context, params *pagination.PaginationParams) (*pagination.PaginatedResult[*item.Item], error)
 }
