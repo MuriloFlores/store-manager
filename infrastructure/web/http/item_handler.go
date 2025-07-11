a3ab3fd8524187285d6c01e94d0df3110dbfb7e3
MuriloFlores <muriloaflore@gmail.com>
2025-07-11 13:15:03 -0300

    debbug

--- infrastructure/web/http/item_handler.go
+++ infrastructure/web/http/item_handler.go
@@ -234,7 +234,8 @@ func (h *ItemHandler) UpdateItem(w http.ResponseWriter, r *http.Request) {
 		MinimumStockLevel: req.MinimumStockLevel,
 	}
 
-	fmt.Println(params.IsActive)
+	fmt.Sprintf("%s", req.Active)
+	fmt.Sprintf("%s", params.IsActive)
 
 	updatedItem, err := h.useCase.Update.Execute(r.Context(), actorIdentity, targetID, params)
 	if err != nil {
