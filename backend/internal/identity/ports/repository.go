197414c0c71a46b02dab32a79fb69d42a9dacff6
murilo flores <muriloaflores@gmail.com>
2026-04-02 22:07:40 -0300

    fix: Change the package name from _common to common

--- backend/internal/identity/ports/repository.go
+++ backend/internal/identity/ports/repository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/MuriloFlores/order-manager/internal/_common"
+	"github.com/MuriloFlores/order-manager/internal/common"
 	"github.com/MuriloFlores/order-manager/internal/identity/domain/entity"
 	"github.com/MuriloFlores/order-manager/internal/identity/domain/vo"
 	"github.com/google/uuid"
@@ -14,7 +14,7 @@ type UserRepository interface {
 	Save(ctx context.Context, user *entity.User) error
 	FindByID(ctx context.Context, id uuid.UUID) (*entity.User, error)
 	FindByEmail(ctx context.Context, email vo.Email) (*entity.User, error)
-	GetUsersInfo(ctx context.Context, roles []vo.Role, pagination _common.Pagination) (*_common.PaginatedResult[*entity.User], error)
+	GetUsersInfo(ctx context.Context, roles []vo.Role, pagination common.Pagination) (*common.PaginatedResult[*entity.User], error)
 	Update(ctx context.Context, user *entity.User) error
 }
 
