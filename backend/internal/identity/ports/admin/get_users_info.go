b6fa55e0e6270c09a4897f232738062573714267
murilo flores <muriloaflores@gmail.com>
2026-04-02 21:54:59 -0300

    fix(identity): fix incorrect import path in GetUsersInfo port

--- backend/internal/identity/ports/admin/get_users_info.go
+++ backend/internal/identity/ports/admin/get_users_info.go
@@ -3,10 +3,10 @@ package admin
 import (
 	"context"
 
-	"github.com/MuriloFlores/order-manager/internal/_common"
+	"github.com/MuriloFlores/order-manager/internal/common"
 	"github.com/MuriloFlores/order-manager/internal/identity/domain/entity"
 )
 
 type GetUsersInfo interface {
-	Execute(ctx context.Context, pagination _common.Pagination, roles []string) (*_common.PaginatedResult[*entity.User], error)
+	Execute(ctx context.Context, pagination common.Pagination, roles []string) (*common.PaginatedResult[*entity.User], error)
 }
