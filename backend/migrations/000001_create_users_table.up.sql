6503d7364eac77979f386dcc815c0f8d7da8d9b1
murilo flores <muriloaflores@gmail.com>
2026-04-05 08:46:35 -0300

    feat(identity): add account lockout and email verification columns to users table

--- backend/migrations/000001_create_users_table.up.sql
+++ backend/migrations/000001_create_users_table.up.sql
@@ -1,10 +1,13 @@
 CREATE TABLE users
 (
-    id       UUID PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    roles    TEXT[]       NOT NULL,
-    active   BOOLEAN      NOT NULL DEFAULT TRUE,
+    id              UUID PRIMARY KEY,
+    username        VARCHAR(255) NOT NULL UNIQUE,
+    password        VARCHAR(255) NOT NULL,
+    roles           TEXT[]       NOT NULL,
+    active          BOOLEAN      NOT NULL DEFAULT TRUE,
+    failed_attempts INT          NOT NULL DEFAULT 0,
+    locked_until    TIMESTAMPTZ,
+    email_verified  bool         NOT NULL DEFAULT FALSE,
 
     CONSTRAINT chk_valid_roles CHECK (roles <@ ARRAY ['ADMIN', 'MANAGER', 'CASHIER', 'STOCK_CLERK']::TEXT[])
 );
